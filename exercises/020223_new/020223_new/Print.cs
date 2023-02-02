d8e8b51d478dee591584b661d775704736e07bc7
Bex <beksen00@gmail.com>
2023-02-02 09:30:25 +0100

    .

---
+++ exercises/020223_new/020223_new/Print.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _020223_printing
+{
+    public class Print
+    {
+        public void PrintArr(int[] ar)
+        {
+            foreach (int i in ar)
+            {
+                Console.WriteLine(i);
+            }
+        }
+
+        public void PrintLast(int[] ar)
+        {
+            Console.WriteLine("last number in the array: " + ar[ar.Length - 1]);
+        }
+
+        public bool IsInArray(int[] arr, int i)
+        {
+            int check = 0;
+            bool isIn = false;
+            while (check < arr.Length)
+            {
+                if (arr[check] == i)
+                {
+                    isIn = true;
+                    break; // break to stop the loop
+                }
+                else
+                {
+                    check++;
+                }
+            }
+            return isIn;
+        }
+    }
+}
---
+++ exercises/020223_new/020223_new/Program.cs
@@ -0,0 +1,22 @@
+﻿
+using System.Runtime.InteropServices;
+
+namespace _020223_printing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] ar = {7,5,3,9,8,2};
+            Print printer = new Print();
+
+            Console.WriteLine("all the numbers in the array:");
+            printer.PrintArr(ar);
+
+            Console.WriteLine();
+            Console.Write("What would you like to check if it's in the array? ");
+            int check = int.Parse(Console.ReadLine());
+            Console.WriteLine(printer.IsInArray(ar, check));
+        }
+    }
+}
