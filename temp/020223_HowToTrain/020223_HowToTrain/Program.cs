d735d513d23967b24c98422df4cb44cc9da5f813
Bex <beksen00@gmail.com>
2023-02-09 07:52:37 +0100

    .

---
+++ temp/020223_HowToTrain/020223_HowToTrain/Program.cs
@@ -0,0 +1,31 @@
+﻿
+
+namespace _020223_HowToTrain
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Test t = new Test();
+            Console.WriteLine(t.FindSmallest(new int[] { -2, 5, 7, 9, -22, 1, 6, 7 }));
+        }
+    }
+
+    class Test
+    {
+        public int FindSmallest(int[] arr)
+        {
+            int i = 0;
+            int smallest = arr[0];
+            while(i < arr.Length)
+            {
+                if (arr[i] < smallest) 
+                {
+                    smallest = arr[i]; 
+                }
+                i++;
+            }
+            return smallest;
+        }
+    }
+}
---
+++ temp/020623_temp/020623_temp/Program.cs
@@ -0,0 +1,38 @@
+﻿
+namespace _020223_temp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] IN = { 1, 2, 3, 3, 2, 1 };
+            int[] IN2 = { 1, 2, 3, 3, 2, 1 };
+
+            Console.WriteLine(Test.mirrorArray(IN));
+            Console.WriteLine(Test.mirrorArray(IN2));
+        }
+    }
+    
+    class Test
+    {
+        public static bool mirrorArray(int[] arr)
+        {
+            int length = arr.Length;
+            for (int i = 0; i < length / 2; i++)
+            {
+                if (arr[i] != arr[length - i - 1])
+                {
+                    return false;
+                }
+            }
+            return true;
+
+            /// 1 2 3 3 2 1
+            /// int lenght = 6;
+            /// arr[0] != arr[6-0-1]
+            /// arr[0] != arr[5] - arr[0] = 1 & arr[5] = 1 
+            /// arr[1] != arr[6-1-1]
+            /// arr[1] != arr[4] - arr[1] = 2 & arr[4] = 2
+        }
+    }
+}
