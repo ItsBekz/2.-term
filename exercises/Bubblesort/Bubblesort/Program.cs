21336799b8a0f5e1a2c320d46fb52029d1ead801
Bex <beksen00@gmail.com>
2023-02-10 07:02:01 +0100

    .

---
+++ exercises/Bubblesort/Bubblesort/Program.cs
@@ -0,0 +1,26 @@
+﻿
+namespace Bubblesort
+{
+    class Program
+    {
+
+        static int[] bubbleSort(int[] arr)
+        {
+            int temp;
+
+            for (int i = 0; i < arr.Length - 1; i++)
+            {
+                for (int j = 0; j < arr.Length - i - 1; j++)
+                {
+                    if (arr[j] > arr[j + 1])
+                    {
+                        temp = arr[j];
+                        arr[j] = arr[j + 1];
+                        arr[j + 1] = temp;
+                    }
+                }
+            }
+            return arr;
+        }
+    }
+}
